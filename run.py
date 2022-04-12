767b23d5b69edcac6c6eb1d2b812b201411141c3
Rodrigo Reis Ferreira <rodrigorf33@gmail.com>
2022-04-11 22:47:55 -0300

    Click count argument in cli using argparse

--- run.py
+++ run.py
@@ -1,6 +1,16 @@
+import argparse
 import time
 
 import pyautogui
+
+#Parse the inputed argument
+parser = argparse.ArgumentParser(description='Muonline clicker')
+parser.add_argument('clickcount', type=int, help='The clickcount param is required.')
+args = parser.parse_args()
+print(f"Click count to be executed: {args.clickcount}")
+
+if args.clickcount <= 0 or args.clickcount > 100000:
+    parser.error("pos_arg cannot be larger than 100k or less than 1")
 
 time.sleep(2)
 screenWidth, screenHeight = pyautogui.size() # Get the size of the primary monitor.
@@ -15,6 +25,6 @@ currentMouseX, currentMouseY = pyautogui.position()
 print(currentMouseX, currentMouseY)
 
 pyautogui.moveTo(currentMouseX, currentMouseY + 220) 
-for cnt in range(1,10):
+for cnt in range(1,3000):
     pyautogui.mouseDown()
     pyautogui.mouseUp()
